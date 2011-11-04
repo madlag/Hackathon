5a9eee797e0718f672c094993e5d06a993789b65
Stéphane JAIS <stephane@missingfeature.fr>
2011-11-04 21:56:01 +0100

    initial project

---
+++ iphone/Hackathon/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Hackathon
+//
+//  Created by Stephane JAIS on 11/4/11.
+//  Copyright (c) 2011 Cantina Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iphone/Hackathon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hackathon
+//
+//  Created by Stephane JAIS on 11/4/11.
+//  Copyright (c) 2011 Cantina Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
